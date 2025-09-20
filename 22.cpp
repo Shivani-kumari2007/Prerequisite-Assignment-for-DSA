d13508d3c7b83d83d46756e9ce467b3df3c9c3c7
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:55:25 +0530

    Create 22.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 22.cpp
@@ -0,0 +1,9 @@
+//Sum of first N natural numbers
+#include <iostream>
+using namespace std;
+int main(){
+    long long n; cin>>n;
+    long long sum = n*(n+1)/2;
+    cout<<sum<<"\n";
+    return 0;
+}
