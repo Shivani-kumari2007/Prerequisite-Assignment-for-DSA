3952e41e7b692ce0ba14e5fad148e776b057c9c0
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:25:38 +0530

    Create 6ascii.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 6ascii.cpp
@@ -0,0 +1,9 @@
+//Write a program to input a character from the user and print its ASCII code
+
+#include <iostream>
+using namespace std;
+int main(){
+    char c; cin>>c;
+    cout << int(c) << "\n";
+    return 0;
+}
