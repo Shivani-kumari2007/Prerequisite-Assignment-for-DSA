3edff3f4a743543c2c3f993e62ea4fe03b3f1691
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:29:35 +0530

    Create 8ascii.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 8ascii.cpp
@@ -0,0 +1,9 @@
+//Write a program to input three characters from the user and display characters with their corresponding ASCII codes.
+
+#include <iostream>
+using namespace std;
+int main(){
+    char a,b,c; cin>>a>>b>>c;
+    cout<<a<<" "<<int(a)<<"\n"<<b<<" "<<int(b)<<"\n"<<c<<" "<<int(c)<<"\n";
+    return 0;
+}
