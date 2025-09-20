2159aa5bacd8581dda43ed0a7d308815570865e5
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:21:06 +0530

    Update simple interest.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

--- simple interest.cpp
+++ simple interest.cpp
@@ -1,1 +1,11 @@
+//Write a program to calculate simple interest. 
 
+#include <iostream>
+using namespace std;
+int main(){
+    double principal, rate, time;
+    cin>>principal>>rate>>time;
+    double si = (principal * rate * time) / 100.0;
+    cout<<si<<"\n";
+    return 0;
+}
