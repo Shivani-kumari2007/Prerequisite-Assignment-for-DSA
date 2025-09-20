66aa5e3c3b6ed8c9125cf0116b88b21d6ee692f0
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:49:05 +0530

    Create 54.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 54.cpp
@@ -0,0 +1,12 @@
+//Centered number pyramid
+#include <iostream>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int s=1;s<=n-i;s++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<j<<" ";
+        cout<<"\n";
+    }
+}
+
