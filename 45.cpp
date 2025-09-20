a4e5ea21dac75e3792e41716ab46cd179f25c7eb
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:42:24 +0530

    Create 45.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 45.cpp
@@ -0,0 +1,12 @@
+//Repeated sequence numbers
+#include <iostream>
+using namespace std;
+int main() {
+    int rows=4, cols=6;
+    for(int i=1;i<=rows;i++) {
+        for(int j=1;j<=cols;j++) cout<<j<<" ";
+        cout<<"\n";
+    }
+}
+
+
