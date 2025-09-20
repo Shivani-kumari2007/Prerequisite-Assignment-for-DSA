021c7be8f7dac9727bd15451e4df6e3f50741f89
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:20:20 +0530

    Update circumference-of-a-circle.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

--- circumference-of-a-circle.cpp
+++ circumference-of-a-circle.cpp
@@ -1,1 +1,10 @@
+//Write a program to calculate circumference of a circle
 
+#include <iostream>
+using namespace std;
+int main(){
+    double r; cin>>r;
+    const double PI = 3.141592653589793;
+    cout << 2*PI*r << "\n";
+    return 0;
+}
