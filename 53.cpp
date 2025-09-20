3ed36a67bf17f8e0aa47bd10f49a7b610a316c3f
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:48:18 +0530

    Create 53.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 53.cpp
@@ -0,0 +1,12 @@
+//Centered pyramid of stars
+#include <iostream>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int s=1;s<=n-i;s++) cout<<" ";
+        for(int j=1;j<=2*i-1;j++) cout<<"*";
+        cout<<"\n";
+    }
+}
+
