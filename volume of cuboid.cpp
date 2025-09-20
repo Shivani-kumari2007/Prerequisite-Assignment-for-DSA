db0b7a2fbebe8703eb8f942bc316210fc667b200
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:23:00 +0530

    Create volume of cuboid.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ volume of cuboid.cpp
@@ -0,0 +1,9 @@
+// Write a program to calculate volume of a cuboid.
+
+#include <iostream>
+using namespace std;
+int main(){
+    double l,w,h; cin>>l>>w>>h;
+    cout << (l*w*h) << "\n";
+    return 0;
+}
