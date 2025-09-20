88f9fcff1539d8fb30041fcf95e7b3de616c2511
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:04:18 +0530

    Create 24.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 24.cpp
@@ -0,0 +1,8 @@
+//Sum of first N odd natural numbers
+#include <iostream>
+using namespace std;
+int main(){
+    long long n; cin>>n; // sum of first n odd numbers = n^2
+    cout<< n*n <<"\n";
+    return 0;
+}
