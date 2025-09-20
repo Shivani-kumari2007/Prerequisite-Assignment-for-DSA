7d7c0364801e63006323b8c4e7c3df882088b5de
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:41:17 +0530

    Create 44.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 44.cpp
@@ -0,0 +1,13 @@
+//Rows with same number
+#include <iostream>
+using namespace std;
+int main() {
+    int rows=4, cols=6;
+    for(int i=1;i<=rows;i++) {
+        for(int j=1;j<=cols;j++) cout<<i<<" ";
+        cout<<"\n";
+    }
+}
+
+
+}
