687bf8496844b62d3d7c67b73f64a96a7e6b4ad2
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:44:31 +0530

    Create 46.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 46.cpp
@@ -0,0 +1,15 @@
+//Swastik pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n=7; // must be odd
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            if(i==0 || i==n-1 || j==0 || j==n-1 || 
+               i==n/2 || j==n/2) cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<"\n";
+    }
+}
+
