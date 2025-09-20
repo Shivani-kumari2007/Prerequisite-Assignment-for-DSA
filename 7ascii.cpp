403d840bd3a44ebe97670f3b5be0920f020a102c
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:26:40 +0530

    Create 7ascii.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 7ascii.cpp
@@ -0,0 +1,10 @@
+//Write a program to input an ASCII code from the user and print its corresponding character.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int code; cin>>code;
+    cout << char(code) << "\n";
+    return 0;
+}
+
