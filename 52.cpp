c041cc2e07d564fe7ff4878b50f5553af3dbbc45
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-21 00:47:39 +0530

    Create 52.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

---
+++ 52.cpp
@@ -0,0 +1,12 @@
+//Inverted left triangle of stars
+#include <iostream>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=n;i>=1;i--) {
+        for(int s=1;s<=n-i;s++) cout<<"  ";
+        for(int j=1;j<=i;j++) cout<<"* ";
+        cout<<"\n";
+    }
+}
+
