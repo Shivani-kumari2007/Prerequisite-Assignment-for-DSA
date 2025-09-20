9bea03819b5cc3f39c3ab423db8c6ced2448ea6d
Shivani kumari <shivanikumari95590@gmail.com>
2025-09-20 23:10:39 +0530

    Update average.cpp
    Signed-off-by: Shivani kumari <shivanikumari95590@gmail.com>

--- average.cpp
+++ average.cpp
@@ -1,1 +1,11 @@
+//Write a program to calculate average of three integers. Numbers are given by the user
 
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c; 
+    cin>>a>>b>>c;
+    double avg = (a + b + c) / 3.0;
+    cout<<avg<<"\n";
+    return 0;
+}
